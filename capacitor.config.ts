0b7901588fe72e0feb2078f683da98907118d042
Moehid23 <muthadoabdul23@gmail.com>
2024-07-03 14:49:06 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LaundryExpo',
+  webDir: 'www'
+};
+
+export default config;
